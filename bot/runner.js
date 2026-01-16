e16a614dcdb612468e4fc511cc6a23809aec2414
Christopher Sardine <christophersardine@Christophers-MacBook-Air.local>
2026-01-16 08:50:25 -0500

    feat(debug): Isolate config.js import

--- bot/runner.js
+++ bot/runner.js
@@ -1,5 +1,5 @@
-/*
 import { loadConfig, requireEnv } from "./config.js";
+/*
 import { createLogger } from "./logger.js";
 import { fetchSamOpportunities } from "./sam_client.js";
 import { normalizeOpportunity } from "./normalizer.js";
@@ -16,8 +16,17 @@ import { buildSlackPayload, postSlackAlert, postSlackMessage } from "./slack.js"
 import { initStorage, upsertOpportunity, getOpportunityState, saveScore, saveAlert } from "./storage.js";
 import { formatDateMMDDYYYY } from "./utils.js";
 */
-console.log("runner.js loaded and imports are commented out");
+console.log("runner.js loaded with config import");
 
-export async function runOpportunityBot() {
-  console.log("runOpportunityBot called with commented out imports");
+export async function runOpportunityBot({
+  dryRun = false,
+  backfillDays = null,
+  configPath = null,
+  fetchImpl = fetch,
+  now = new Date(),
+  verbose = false,
+} = {}) {
+  const { config } = loadConfig(configPath);
+  console.log("runOpportunityBot called with config import");
+  console.log("Config loaded:", config);
 }
