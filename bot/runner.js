b9bf06b51f32ba3fb4b6b03281bd72c693532d9e
Christopher Sardine <christophersardine@Christophers-MacBook-Air.local>
2026-01-16 09:27:23 -0500

    feat(debug): Isolate scoring.js import

--- bot/runner.js
+++ bot/runner.js
@@ -3,7 +3,6 @@ import { createLogger } from "./logger.js";
 import { fetchSamOpportunities } from "./sam_client.js";
 import { normalizeOpportunity } from "./normalizer.js";
 import { fetchDescriptionText } from "./enrich.js";
-/*
 import {
   buildOpportunityHash,
   deterministicScore,
@@ -11,12 +10,13 @@ import {
   shouldAlert,
   buildFallbackScore,
 } from "./scoring.js";
+/*
 import { scoreWithAi } from "./ai.js";
 import { buildSlackPayload, postSlackAlert, postSlackMessage } from "./slack.js";
 import { initStorage, upsertOpportunity, getOpportunityState, saveScore, saveAlert } from "./storage.js";
 import { formatDateMMDDYYYY } from "./utils.js";
 */
-console.log("runner.js loaded with normalizer and enrich imports");
+console.log("runner.js loaded with scoring import");
 
 export async function runOpportunityBot({
   dryRun = false,
@@ -28,6 +28,6 @@ export async function runOpportunityBot({
 } = {}) {
   const { config } = loadConfig(configPath);
   const logger = createLogger({ verbose });
-  console.log("runOpportunityBot called with normalizer and enrich imports");
-  logger.info("Config, logger, sam_client, normalizer, and enrich loaded successfully");
+  console.log("runOpportunityBot called with scoring import");
+  logger.info("...and scoring loaded successfully");
 }
