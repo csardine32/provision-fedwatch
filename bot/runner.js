a936e1af5ea9e00550ddedc97f132cb884d9ee8f
Christopher Sardine <christophersardine@Christophers-MacBook-Air.local>
2026-01-16 08:46:35 -0500

    feat(debug): Comment out all imports in runner.js

--- bot/runner.js
+++ bot/runner.js
@@ -1,4 +1,23 @@
-console.log("runner.js loaded");
+/*
+import { loadConfig, requireEnv } from "./config.js";
+import { createLogger } from "./logger.js";
+import { fetchSamOpportunities } from "./sam_client.js";
+import { normalizeOpportunity } from "./normalizer.js";
+import { fetchDescriptionText } from "./enrich.js";
+import {
+  buildOpportunityHash,
+  deterministicScore,
+  scoreToLabel,
+  shouldAlert,
+  buildFallbackScore,
+} from "./scoring.js";
+import { scoreWithAi } from "./ai.js";
+import { buildSlackPayload, postSlackAlert, postSlackMessage } from "./slack.js";
+import { initStorage, upsertOpportunity, getOpportunityState, saveScore, saveAlert } from "./storage.js";
+import { formatDateMMDDYYYY } from "./utils.js";
+*/
+console.log("runner.js loaded and imports are commented out");
+
 export async function runOpportunityBot() {
-  console.log("runOpportunityBot called");
+  console.log("runOpportunityBot called with commented out imports");
 }
